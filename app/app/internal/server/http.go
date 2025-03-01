20f3a03eb0689f591208cb8c372a45faa5846105
wangjiabao <840028401@qq.com>
2025-03-01 11:52:29 +0800

    ok

--- app/app/internal/server/http.go
+++ app/app/internal/server/http.go
@@ -61,6 +61,7 @@ func NewWhiteListMatcher() selector.MatchFunc {
 	whiteList["/api.App/Deposit3"] = struct{}{}
 	whiteList["/api.App/Deposit4"] = struct{}{}
 	whiteList["/api.App/AdminTrade"] = struct{}{}
+	whiteList["/api.App/AdminCreateGoods"] = struct{}{}
 	whiteList["/api.App/AdminDailyLocationRewardNew"] = struct{}{}
 	whiteList["/api.App/AdminDailyLocationReward"] = struct{}{}
 	whiteList["/api.App/AdminDailyAreaReward"] = struct{}{}
